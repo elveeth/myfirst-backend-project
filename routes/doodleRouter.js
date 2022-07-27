a037f2219bc14cf2c709949d6a5f08d2881446f6
elveeth <logan.veith@gmail.com>
2022-07-26 23:04:16 -0700

    added doodle router

---
+++ routes/doodleRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const doodleRouter = express.Router();
+
+doodleRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will add doodle to page.');
+})
+.post((req, res) => {
+    res.end(`Will add the doodle: ${req.body.name} with description: ${req.body.description}, and the jpg file.`);
+})
+.put((req, res) => {
+    res.end(`Will update doodle: ${req.body.name} or description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end('Deleting doodle');
+});
+
+module.exports = doodleRouter;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const doodleRouter = require('./routes/doodleRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+
+app.use('/doodles', doodleRouter);
 
 app.use(express.static(__dirname + '/public'));
 
